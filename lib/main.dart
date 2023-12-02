d3478a729f78a32922830e0d9f88e357b393af1f
Alex Feldmeier <alexdfeldmeier@gmail.com>
2023-12-02 00:18:06 -0600

    Added dependencies to provider

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weight_tracker/data_sources/auth_data_source.dart';
+import 'package:weight_tracker/data_sources/weight_data_source.dart';
 import 'package:weight_tracker/firebase_options.dart';
 import 'package:weight_tracker/repositories/auth_repository.dart';
+import 'package:weight_tracker/repositories/weight_repository.dart';
 import 'package:weight_tracker/router.dart';
 
 void main() async {
@@ -41,12 +43,22 @@ class MyApp extends StatelessWidget {
             context.read<FirebaseAuth>(),
           ),
         ),
+        Provider(
+          create: (_) => WeightDataSource(
+            context.read<FirebaseFirestore>(),
+          ),
+        ),
         //
         // ----- Repositories -----
         //
         Provider(
           create: (_) => AuthRepository(
             context.read<AuthDataSource>(),
+          ),
+        ),
+        Provider(
+          create: (_) => WeightRepository(
+            context.read<WeightDataSource>(),
           ),
         ),
       ],
